0ccaa8e064f788597b95cc342626b2c9cb0f836f
dolbex <gary@lbm.co>
2022-07-11 08:10:09 -0400

    tweaks to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,14 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  alias: {
+    "@vue/devtools-api": "@vue/devtools-api",
+  },
   css: ["~/assets/css/tailwind.css"],
   build: {
     postcss: {
       postcssOptions: require("./postcss.config.js"),
     },
+    transpile: ["@heroicons/vue", "@headlessui/vue"],
   },
 });
